09e13e7e5b02660f965b0c3e9b94e03812beb8f0
Ilyas Yermekbayev <ermekbaevilias12@gmail.com>
2026-01-21 12:50:41 +0500

    Ilyas Yermekbayev

--- src/edu/aitu/oop3/services/ReservationService.java
+++ src/edu/aitu/oop3/services/ReservationService.java
@@ -1,6 +1,19 @@
 package edu.aitu.oop3.services;
 
+import edu.aitu.oop3.repositories.ReservationRepository;
+import edu.aitu.oop3.services.exceptions.RoomNotAvailableException;
 import edu.aitu.oop3.services.interfaces.ReservationServiceInterface;
 
+import java.time.LocalDate;
+
 public class ReservationService implements ReservationServiceInterface {
+    private final ReservationRepository reservationRepository;
+    public ReservationService(ReservationRepository reservationRepository) {
+        this.reservationRepository = reservationRepository;
 }
+public void createReservation(int room_id, LocalDate check_in, LocalDate check_out) {
+    if(!reservationRepository.isRoomAvailable(room_id,check_in,check_out)) {
+        throw new RoomNotAvailableException("Room" + room_id + "is not available from" + check_in +     "to" + check_out);
+        }
+    }
+ }
